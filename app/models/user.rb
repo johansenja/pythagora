c69d77b03cf97ba896f76b5ce1d7253c2c8f22eb
Joseph Johansen <jo.johansen@hotmail.com>
2018-11-12 16:07:00 +0000

    initial validations and some security

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  # one or more of anything except @; then @; then one or more of anything but @ or .;
+  # one or more of anything except @
+  validates :email, format: { with: /[^@]{1,}\@[^@.]{1,}\.[^@]{1,}/ }, uniqueness: true
 end
