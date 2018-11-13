cd22ad889617581e5bd13b8944de8cd254b3aacf
Joseph Johansen <jo.johansen@hotmail.com>
2018-11-13 15:18:57 +0000

    added users controller and updated a few routes'

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :jobs
+  resources :users, only: [:index, :show] do
+    get '/buyer_dashboard', to: 'jobs#buyer_jobs'
+    get '/developer_dashboard', to: 'jobs#developer_jobs'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
